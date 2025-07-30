fe7c0fb8c275148bbaec12ff7c832f74fb27c1c1
alexpivvit <alex@pivvit.com>
2025-07-30 19:13:05 +0300

    feat: add validation

--- go.mod
+++ go.mod
@@ -9,8 +9,14 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.27.0 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -3,26 +3,86 @@ package auth
 import (
 	"context"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/ozaitsev92/ssoprotos/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type serverAPI struct {
 	sso.UnimplementedAuthServer
+	validate *validator.Validate
 }
 
 func Register(gRPC *grpc.Server) {
-	sso.RegisterAuthServer(gRPC, &serverAPI{})
+	sso.RegisterAuthServer(gRPC, &serverAPI{
+		validate: validator.New(),
+	})
 }
 
-func (s *serverAPI) Register(context.Context, *sso.RegisterRequest) (*sso.RegisterResponse, error) {
-	return nil, nil // TODO: Implement registration logic
+type registerInput struct {
+	Email    string `validate:"required,email"`
+	Password string `validate:"required,min=8"`
 }
 
-func (s *serverAPI) Login(context.Context, *sso.LoginRequest) (*sso.LoginResponse, error) {
-	return nil, nil // TODO: Implement login logic
+func (s *serverAPI) Register(ctx context.Context, req *sso.RegisterRequest) (*sso.RegisterResponse, error) {
+	input := registerInput{
+		Email:    req.GetEmail(),
+		Password: req.GetPassword(),
+	}
+
+	if err := s.validate.Struct(input); err != nil {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			"validation failed: %v",
+			err.Error(),
+		)
+	}
+
+	return &sso.RegisterResponse{}, nil // TODO: Implement login logic
 }
 
-func (s *serverAPI) IsAsmin(context.Context, *sso.IsAdminRequest) (*sso.IsAdminResponse, error) {
-	return nil, nil // TODO: Implement admin check logic
+type loginInput struct {
+	Email    string `validate:"required,email"`
+	Password string `validate:"required,min=8"`
+	AppId    int32  `validate:"required"`
+}
+
+func (s *serverAPI) Login(ctx context.Context, req *sso.LoginRequest) (*sso.LoginResponse, error) {
+	input := loginInput{
+		Email:    req.GetEmail(),
+		Password: req.GetPassword(),
+		AppId:    req.GetAppId(),
+	}
+
+	if err := s.validate.Struct(input); err != nil {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			"validation failed: %v",
+			err.Error(),
+		)
+	}
+
+	return &sso.LoginResponse{}, nil // TODO: Implement login logic
+}
+
+type isAdminInput struct {
+	UserId int64 `validate:"required"`
+}
+
+func (s *serverAPI) IsAsmin(ctx context.Context, req *sso.IsAdminRequest) (*sso.IsAdminResponse, error) {
+	input := isAdminInput{
+		UserId: req.GetUserId(),
+	}
+
+	if err := s.validate.Struct(input); err != nil {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			"validation failed: %v",
+			err.Error(),
+		)
+	}
+
+	return &sso.IsAdminResponse{IsAdmin: true}, nil // TODO: Implement admin check logic
 }
