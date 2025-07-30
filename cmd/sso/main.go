04a8398930b83aed4cb41ad45ff58da37e083b51
alexpivvit <alex@pivvit.com>
2025-07-30 17:08:24 +0300

    feat: create config parser and initialize logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,5 +1,49 @@
-package sso
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/ozaitsev92/sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+)
 
 func main() {
+	cfg := config.MustLoad()
 
+	log := setupLogger(cfg.Env)
+
+	log.Info(
+		"starting SSO service",
+		slog.String("env", cfg.Env),
+		slog.Int("grpc_port", cfg.GRPC.Port),
+	)
+
+	// todo: init app
+
+	// todo: start grpc server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		}))
+	default:
+		panic(fmt.Sprintf("unknown environment: %s", env))
+	}
+
+	return log
 }
